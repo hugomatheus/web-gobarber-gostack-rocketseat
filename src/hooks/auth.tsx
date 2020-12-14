c49c932454c6001e829c3ba7e24dda36bb81dbd5
hugomatheus <hg.targino@gmail.com>
2020-12-13 22:25:46 -0300

    Dashboard - Próximo agendamento

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,9 +1,14 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -11,7 +16,7 @@ interface SignInCredentials {
   password: string;
 }
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,66 @@
 import React from 'react';
+
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
+
+import logoImage from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
-// import { Container } from './styles';
-
 const Dashboard: React.FC = () => {
-  const { user } = useAuth();
-  return <div>Dashboard</div>;
+  const { signOut, user } = useAuth();
+  const { id, name, avatar_url } = user;
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImage} alt="GoBarber" />
+
+          <Profile>
+            <img src={avatar_url} alt={name} />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src={avatar_url} alt="Hugo" />
+              <strong>Hugo</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
